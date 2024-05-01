d8ef6c1b4fae701c88f1d755fb690b8de0af4648
Curzey <carstena1994@gmail.com>
2024-05-01 12:02:52 +0200

    adjust usp list settings

--- extensions/checkout-usp-list/src/Checkout.jsx
+++ extensions/checkout-usp-list/src/Checkout.jsx
@@ -12,33 +12,18 @@ export { checkoutBlock };
 
 function App() {
   const {
-    text, icon,
-    text2, icon2,
-    text3, icon3,
+    text,
+    text2,
+    text3
   } = useSettings();
 
   return (
-    <List marker="none">
-      {text ??
-        <ListItem>
-          <Icon source={icon} />
-          {text}
-        </ListItem>
-      }
-
-      {text2 ??
-        <ListItem>
-          <Icon source={icon2} />
-          {text2}
-        </ListItem>
-      }
-
-      {text3 ??
-        <ListItem>
-          <Icon source={icon3} />
-          {text3}
-        </ListItem>
-      }
-    </List>
+    <>
+      <List>
+        {text ?? <ListItem>USP12</ListItem>}
+        {text2 ?? <ListItem>USP2</ListItem>}
+        {text3 ?? <ListItem>USP3</ListItem>}
+      </List>
+    </>
   );
 }
